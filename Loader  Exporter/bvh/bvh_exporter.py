f58760588ec40cce2dd1b978ace22235ebef58aa
YuSuzu1107 <yu20021107@gmail.com>
2024-06-27 19:03:54 +0900

    Update bvh_exporter.py

--- Loader  Exporter/bvh/bvh_exporter.py
+++ Loader  Exporter/bvh/bvh_exporter.py
@@ -51,11 +51,11 @@ class BVHExporter:
             file.write(f"{indent}}}\n")
 
 # 使用例
-loader = BVHLoader('../BEAT/1_wayne_1_11_12.bvh')
+loader = BVHLoader('../data/1_wayne_1_11_12.bvh')
 loader.load()
 
 if loader.root is None:
     raise ValueError("Root node is None. Cannot export BVH file.")
 
 exporter = BVHExporter(loader.root, loader.frames, loader.frame_time)
-exporter.export('../BEAT/exported_file.bvh')
+exporter.export('../data/exported_file.bvh')
